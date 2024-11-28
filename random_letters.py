a3e5e756d4e4eb8efd8d90dc59c0e71ec2044f2f
Napoleao1 <ernaninandoxd@gmail.com>
2024-11-28 17:25:11 -0300

    created placeholder

--- random_letters.py
+++ random_letters.py
@@ -4,12 +4,20 @@ lista_palavras = ["camel", "monkey", "cat"]
 chosen_word = random.choice(lista_palavras)
 print(chosen_word)
 
-usuario_input = input("escolha uma letra: ").lower()
-print(usuario_input)
+#criando string com espaço em branco
+placeholder = "_"
+word_lenght = len(chosen_word) #criando a váriavel para contar as palavras
+for position in range(word_lenght): # loop para adicinar os espaços em brancos em qualquer palavra da lista
+    placeholder += "_" 
+print(placeholder)
+
+escolha = input("escolha uma letra: ").lower()
+print(escolha)
+
 
 
 for letra in chosen_word:
-    if letra == usuario_input:
+    if  letra == escolha:
         print("right")
     else:
-       print("wrong")
+        print("wrong")
