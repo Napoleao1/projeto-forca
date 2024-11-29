ed7bddb22b5549ba8938b1482bbf247fe688b4a4
Napoleao1 <ernaninandoxd@gmail.com>
2024-11-28 23:18:25 -0300

    criando lógica do loop de escolha

--- random_letters.py
+++ random_letters.py
@@ -6,8 +6,12 @@ print(chosen_word)
 
 #criando string com espaço em branco
 placeholder = "_"
+
 word_lenght = len(chosen_word) #criando a váriavel para contar as palavras
-for position in range(word_lenght): # loop para adicinar os espaços em brancos em qualquer palavra da lista
+
+# loop para adicinar os espaços em brancos em qualquer palavra da lista
+for position in range(word_lenght):
+    
     placeholder += "_" 
 print(placeholder)
 
